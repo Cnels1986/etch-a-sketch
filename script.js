c581dcf99913cf187dda7823da63d50303a326b1
Chris Nelson <CNels1986@gmail.com>
2021-11-30 17:52:04 -0500

    layout is created and currently working for black and white. Reset button added in addition

--- script.js
+++ script.js
@@ -5,10 +5,39 @@ let total = count * count;
 
 const container = document.querySelector('#container');
 
+const reset = document.querySelector('#reset');
+// reset.onclick = () => loadContainer();
+reset.addEventListener('click', () =>{
+    resetContainer();
+});
+
 // finds the percentage each block should be relative to the main container
 function findPercentage(){
     let temp = 500/count;
     return (temp/500) * 100;
 }
 
-console.log(findPercentage(count + "%"));
+function loadContainer(){
+    for(let a = 0; a < total; a++){
+        const block = document.createElement('div');
+        let percentage = findPercentage().toString() + '%';
+
+        block.style.height = percentage;
+        block.style.width = percentage;
+        block.classList.add('block');
+        block.addEventListener('mouseover', () =>{
+            block.style.backgroundColor = 'black';
+        });
+        container.appendChild(block);
+    }
+}
+
+
+function resetContainer(){
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach((block) => {
+        block.style.backgroundColor = 'white';
+    });
+}
+
+loadContainer();
