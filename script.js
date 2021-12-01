6992b94b7c74d1da38a984fd41022c91ce79ee10
Chris Nelson <CNels1986@gmail.com>
2021-12-01 17:02:58 -0500

    able to toggle with mode button is selected, normal and eraser mode functioning properly

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let count = 16;
 // total number of blocks based on the selected count
 let total = count * count;
 let color = '#000000';
+// Stores the current mode selected (1 - normal, 2 - grayscale, 3 - rainbow, 4 - eraser)
+let modeSelected = 1;
 
 
 const container = document.querySelector('#container');
@@ -13,12 +15,20 @@ reset.addEventListener('click', () =>{
     resetContainer();
 });
 
+const modes = document.querySelectorAll('.btn');
+modes.forEach((mode) => {
+    mode.addEventListener('click', () => {
+        changeMode(mode.value);
+    });
+});
+
 // finds the percentage each block should be relative to the main container
 function findPercentage(){
     let temp = 500/count;
     return (temp/500) * 100;
 }
 
+// loads the container grid with divs based on the count value from the slider
 function loadContainer(){
     for(let a = 0; a < total; a++){
         const block = document.createElement('div');
@@ -35,17 +45,18 @@ function loadContainer(){
     }
 }
 
+// updates the count and total when needed
 function getCount(){
     const slider = document.getElementById('blockRange');
     const num = document.getElementById('slideCount');
     let numBlocks = slider.value;
-    num.innerHTML = numBlocks;
+    num.innerHTML = numBlocks + " x " + numBlocks;
     // updates count and total count of the blocks
     count = numBlocks;
     total = numBlocks * numBlocks;
 }
 
-
+// clears the container of divs then calls loadContainer() to refill it
 function resetContainer(){
     const blocks = document.querySelectorAll('.block');
     blocks.forEach((block) => {
@@ -55,9 +66,48 @@ function resetContainer(){
     loadContainer();
 }
 
-const colorValue = document.getElementById('color')
+function changeMode(mode){
+    // checks if button clicked will change to a different mode, if not function does not change anything (same mode was selected)
+    if(mode != modeSelected){
+        const btns = document.querySelectorAll('.btn');
+        btns.forEach((btn) => {
+            btn.classList.remove('toggled');
+        });
+        switch(mode){
+            case '1':
+            const a = document.getElementById('normBut');
+            a.classList.add('toggled');
+            modeSelected = 1;
+            const colorValue = document.getElementById('color');
+            color = colorValue.value;
+            break;
+            case '2':
+            const b = document.getElementById('grayBut');
+            b.classList.add('toggled');
+            modeSelected = 2;
+            console.log('test');
+            break;
+            case '3':
+            const c = document.getElementById('rainbowBut');
+            c.classList.add('toggled');
+            modeSelected = 3;
+            break;
+            case '4':
+            const d = document.getElementById('eraserBut');
+            d.classList.add('toggled');
+            modeSelected = 4;
+            color = '#ffffff';
+            break;
+        }
+    }
+}
+
+// updates the color picker's hex value to change what colors the divs turn to
+const colorValue = document.getElementById('color');
 colorValue.oninput = function() {
+    if(modeSelected != 4){
         color = colorValue.value;
+    }
 };
 
 loadContainer();
@@ -66,8 +116,10 @@ loadContainer();
 const slider = document.getElementById('blockRange');
 const num = document.getElementById('slideCount');
 slider.oninput = function() {
-    num.innerHTML = this.value;
+    num.innerHTML = this.value + " x " + this.value;
 };
+// resets the grid of blocks with the updated value from the slider
 slider.onchange = function(){
+    num.innerHTML = this.value + " x " + this.value;
     resetContainer();
 };
